f01e7d8b599b70c945fc57a928aa789cb6365394
Miguel Cervantes <miguel.cervantes@izenda.com>
2020-07-13 15:59:30 -0400

    Initial commit with .NET Core project

---
+++ IzendaCustomBootstrapper/CustomBootstrapper.cs
@@ -0,0 +1,74 @@
+﻿using Izenda.BI.API.Bootstrappers;
+using Izenda.BI.Framework.Models;
+using Izenda.BI.Framework.Models.Common;
+using Izenda.BI.Framework.Models.Paging;
+using IzendaCustomBootstrapper.Extensions;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Responses;
+using Nancy.TinyIoc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IzendaCustomBootstrapper
+{
+    /// <summary>
+    /// Custom Bootstrapper to hook into the NancyFx API pipeline
+    /// </summary>
+    public class CustomBootstrapper : IzendaBootstraper
+    {
+        const string ApiPrefix = "api";
+
+        private JsonSerializerSettings _serializer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomBootstrapper"/> class.
+        /// </summary>
+        public CustomBootstrapper()
+            : base()
+        {
+            _serializer = new JsonSerializerSettings
+            {
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+            };
+        }
+
+        /// <summary>
+        /// Allows for pre/post request hooks into the API endpoints
+        /// </summary>
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            pipelines.AfterRequest.AddItemToEndOfPipeline(ctx =>
+            {
+                this.ModifyExportResponse(ctx);
+            });
+
+            base.RequestStartup(container, pipelines, context);
+        }
+
+        /// <summary>
+        /// Modifies the reponse of the export endpoints
+        /// </summary>
+        /// <param name="ctx">The Nancy Context</param>
+        private void ModifyExportResponse(NancyContext ctx)
+        {
+            if (!(ctx.Request.Url.Path.Contains($"/{ApiPrefix}/export/pdf") || ctx.Request.Url.Path.Contains($"/{ApiPrefix}/export/word") || ctx.Request.Url.Path.Contains($"/{ApiPrefix}/export/excel")))
+                return;
+
+            if (ctx?.Response?.Headers != null && ctx.Response.Headers.Any())
+            {
+                var headerValue = ctx.Response.Headers.First().Value;
+                var ogFileName = headerValue.Contains("filename") ? headerValue.Substring(headerValue.LastIndexOf("filename=") + 9) : null;
+                if (!string.IsNullOrWhiteSpace(ogFileName))
+                {
+                    var lastIndex = ogFileName.LastIndexOf('.');
+                    var newFileName = $"{ogFileName.Substring(0, lastIndex)}_{DateTime.Now.ToString("yyyy-MM-dd_HH-mm")}{ogFileName.Substring(lastIndex)}";
+                    ctx.Response.AsAttachment(string.IsNullOrWhiteSpace(newFileName) ? ogFileName : newFileName);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Izenda Custom Bootstrapper
+
+ :warning: **This project is designed for demonstration purposes, please ensure that security and customization meet the standards of your company.**
+ 
+ 
+## Overview
+A custom bootstrapper allows you to modify requests and responses to the Izenda API (.NET Core resources). 
+
+## A Few Uses Cases
+1. Filtering down a large list of tenants based on some custom criteria.
+2. Removing items from filter field data.
+
+Currently, this repo only shows how to remove items from filter field data.
+
+## Required References:
+
+1. Izenda.BI.API.dll  
+2. Izenda.BI.Framework.dll 
+3. Nancy.dll
+
+## Installation
+
+1. Build the project and copy the DLL into the Izenda API directory along with the other Izenda API DLL files.
+  
+   
+2. Add the following key/values to the appSettings.json (API) file to use the custom bootstrapper.
+```
+  "izenda.nancyfx.bootstrapper.dll": "IzendaCustomBootstrapper.dll",
+  "izenda.nancyfx.bootstrapper.type": "IzendaCustomBootstrapper.CustomBootstrapper",
+```
+4. Restart the API instance
